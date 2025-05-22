1363add488ffb1ae2cafbf1755b322d1f4965b9e
Kurt Garloff <kurt@garloff.de>
2025-05-22 13:07:01 +0000

    Fix typo in recommendation.
    Signed-off-by: Kurt Garloff <kurt@garloff.de>

--- 01-kind-cluster.sh
+++ 01-kind-cluster.sh
@@ -5,7 +5,8 @@ DEV=$(ip route show default | head -n1 | sed 's/^.*dev \([^ ]*\).*$/\1/')
 CLOUDMTU=$(ip link show $DEV | head -n1 | sed 's/^.*mtu \([0-9]*\) .*$/\1/')
 DOCKERMTU=$(ip link show docker0 | head -n1 | sed 's/^.*mtu \([0-9]*\) .*$/\1/')
 if test $((DOCKERMTU+50)) -gt $CLOUDMTU; then
-	echo "WARNING: Consider ip link set deb docker0 mtu $((CLOUDMTU-50))"
+	echo "WARNING: Consider ip link set dev docker0 mtu $((CLOUDMTU-50))"
+	echo "   ... and you may want to do the same for kind's bridge device br-*"
 fi
 # Create kind cluster
 if test "$(kind get clusters)" != "kind"; then
