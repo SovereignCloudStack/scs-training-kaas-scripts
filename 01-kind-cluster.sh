6a8dd3212115f5062549c4427f2d3fc8c217e06b
Kurt Garloff <kurt@garloff.de>
2025-05-22 09:57:02 +0000

    Detect MTU and issue warning.
    Also don't error out on already running kind.
    
    Signed-off-by: Kurt Garloff <kurt@garloff.de>

--- 01-kind-cluster.sh
+++ 01-kind-cluster.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 set -e
+# Detect MTU of interface with default route
+DEV=$(ip route show default | head -n1 | sed 's/^.*dev \([^ ]*\).*$/\1/')
+CLOUDMTU=$(ip link show $DEV | head -n1 | sed 's/^.*mtu \([0-9]*\) .*$/\1/')
+DOCKERMTU=$(ip link show docker0 | head -n1 | sed 's/^.*mtu \([0-9]*\) .*$/\1/')
+if test $((DOCKERMTU+50)) -gt $CLOUDMTU; then
+	echo "WARNING: Consider ip link set deb docker0 mtu $((CLOUDMTU-50))"
+fi
 # Create kind cluster
-kind create cluster
+if test "$(kind get clusters)" != "kind"; then
+	kind create cluster
+else
+	echo "kind cluster already running"
+fi
